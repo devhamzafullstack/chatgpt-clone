4ecc9e8bb64900cd1fc2ceaac53f2c59d4941020
DevVain <InsaneVain786@gmail.com>
2025-03-07 05:54:28 +0000

    update

--- backend/index.js
+++ backend/index.js
@@ -19,12 +19,18 @@ const imagekit = new ImageKit({
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use(express.json({ limit: "10mb" }));
 
